95998527b72d27d5ae608b2019a0e2b82e4f29d2
ThomCoder <43434564+ThomCoder@users.noreply.github.com>
2021-08-15 19:40:31 +0200

    #3: Add Systemconfig as parameter
    This is required since we have to know via which program
    we have to install packages.

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -1,10 +1,11 @@
 use crate::packageconfig::Package;
+use crate::systemconfig::Systemconfig;
 
 pub mod linux;
 pub mod mac;
 pub mod windows;
 
 pub trait Installer {
-    fn install_package(package: &Package);
-    fn install_package_list(packages: Vec<Package>);
+    fn install_package(package: &Package, systemconfig: &Systemconfig);
+    fn install_package_list(packages: Vec<Package>, systemconfig: &Systemconfig);
 }
--- src/backends/windows.rs
+++ src/backends/windows.rs
@@ -1,9 +1,10 @@
 use crate::backends::Installer;
 use crate::packageconfig::Package;
+use crate::systemconfig::Systemconfig;
 
 struct WindowsBackend;
 
 impl Installer for WindowsBackend {
-    fn install_package(package: &Package) {}
-    fn install_package_list(packages: Vec<Package>) {}
+    fn install_package(package: &Package, systemconfig: &Systemconfig) {}
+    fn install_package_list(packages: Vec<Package>, systemconfig: &Systemconfig) {}
 }
