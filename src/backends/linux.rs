b5ec3903067d0b91dd3513695656c429a94f4e80
ThomCoder <43434564+ThomCoder@users.noreply.github.com>
2021-10-02 17:16:54 +0200

    #5: Populate function handle_web_install

--- src/backends/linux.rs
+++ src/backends/linux.rs
@@ -4,6 +4,9 @@ use crate::packageconfig::Status;
 use crate::packageconfig::Source;
 use crate::systemconfig::Systemconfig;
 use std::process::Command;
+use curl::easy::Easy;
+use std::fs::File;
+use std::io::Write;
 
 pub struct LinuxBackend;
 
@@ -70,8 +73,62 @@ fn handle_local_install(package: &mut Package, systemconfig: &Systemconfig) -> R
 
 fn handle_web_install(package: &mut Package, systemconfig: &Systemconfig) -> Result<(), ()> {
 	dbg!("handle_web_install!");
+	dbg!(systemconfig.install_cmd.clone());
+	dbg!(package.name.clone());
+	dbg!(package.source.as_ref().unwrap().0.clone());
+	dbg!(package.source.as_ref().unwrap().1.clone());
+	dbg!(package.cmd.as_ref());
 
-	Err(())
+	// Extract filename from url
+	let source_dict = package.source.as_ref().unwrap();
+	let source_owned = source_dict.to_owned();
+	let url = source_owned.1.clone();
+	let file_name = url.split('/').last().unwrap_or("foo.bin");
+
+	let file = File::create(file_name);
+
+	match file {
+		Ok(mut f) => {
+			let mut easy = Easy::new();
+			easy.url(package.source.as_ref().unwrap().1.clone().as_str()).unwrap();
+
+			easy.write_function(move |data| {
+				let _ = f.write_all(data);
+				Ok(data.len())
+			}).unwrap();
+
+			easy.perform().unwrap();
+
+			println!("{}", easy.response_code().unwrap());
+		}
+
+		Err(e) => {
+			dbg!(e);
+			return Err(());
+		}
+	}
+
+	let split_cmd = systemconfig.install_cmd.split_whitespace();
+	let parts: Vec<&str> = split_cmd.collect();
+
+	// Extract the actual arguments from the full install cmd
+	let mut args: Vec<&str> = parts.clone();
+	args.remove(0);
+
+	let output = Command::new(parts[0])
+		.args(args)
+		.arg(file_name)
+		.output()
+		.unwrap();
+
+	if output.status.success() {
+		package.status = Status::Succeeded;
+		Ok(())
+	} else {
+		package.status = Status::Failed;
+		dbg!(output.status.to_string());
+		Err(())
+	}
 }
 
 impl Installer for LinuxBackend {
